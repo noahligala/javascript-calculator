08d5f67931b4bd7b377c3d17ba21c8aac0107607
Noah Ligala <nligala7@gmail.com>
2021-07-27 20:00:22 +0300

    corrected multiplication

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ class Calculator {
       case '-':
         computation = prev - current
         break
-      case 'X':
+      case 'x':
         computation = prev * current
         break
       case '/':
